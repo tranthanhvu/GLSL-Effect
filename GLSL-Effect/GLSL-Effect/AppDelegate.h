aa103e01617a5c106aaaf6b1316c7696e85a267f
Thanh Vu <ttvu90@gmail.com>
2013-05-16 15:52:39 +0700

    init project
    first init

---
+++ GLSL-Effect/GLSL-Effect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLSL-Effect
+//
+//  Created by yoyo on 5/16/13.
+//  Copyright (c) 2013 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GLSL-Effect/GLSL-Effect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLSL-Effect
+//
+//  Created by yoyo on 5/16/13.
+//  Copyright (c) 2013 yoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
